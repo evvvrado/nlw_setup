d676c9f8ddcc2f94c3c6513e0801776d953e80d2
Everaldo Júnior <everaldocrj@gmail.com>
2023-01-23 16:00:20 -0300

    🎃 [ 02 ] - [ Ready SERVER. Second Web Class Finished ]

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,14 @@
-import Fastify from "fastify"
-import cors from "@fastify/cors"
-import { PrismaClient } from "@prisma/client"
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+import { appRoutes } from "./routes"
 
 const app = Fastify()
-const prisma = new PrismaClient()
 
 app.register(cors)
-
-app.get("/", () => {
-	return "Hello World"
-})
+app.register(appRoutes)
 
 app.listen({
-	port: 3333,
+  port: 3333,
 }).then(() => {
-	console.log("Servidor Iniciado!")
+  console.log('HTTP Server running!')
 })
